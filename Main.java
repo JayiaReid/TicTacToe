f28b4e0338bfc3771a4c5e984ccab2f61bb01c63
JayiaReid <155191815+JayiaReid@users.noreply.github.com>
2024-01-19 17:20:10 -0500

    Update and rename TicTacGame.java to Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package tictactoe;
-/**
- *
- * @author jayia
- */
-import java.util.Scanner;
 
+package tictactoe;
+import java.util.Scanner;
 
 public class TicTacGame{
      private static boolean gameover = false;
